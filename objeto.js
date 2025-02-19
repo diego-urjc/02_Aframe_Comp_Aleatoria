389b2425ea915e77d7b99e5abd7dcc43353fee94
Diego Lucas Luis <d.lucas.2021@alumnos.urjc.es>
2025-02-19 21:49:44 +0100

    Fase 1

---
+++ objeto.js
@@ -0,0 +1,16 @@
+AFRAME.registerComponent('objeto', {
+    schema: {type: 'string', default: 'cubo'},
+    init: function () {
+        console.log('objeto (init)');
+
+        this.primitives = {'cubo': 'box', 'cilindro': 'cylinder', 'esfera': 'sphere'};
+        let primitive = this.primitives[this.data];      
+
+        this.hijo = document.createElement('a-entity');
+        this.hijo.setAttribute('geometry', {primitive: primitive});
+        this.hijo.setAttribute('material', {});
+        this.el.appendChild(this.hijo);
+    },
+    
+});
+
